e3f5653978a501a9c0c4cffdec56f445de5941d5
snaven10 <funes@samuelfunes.com>
2025-01-09 22:27:23 -0600

    Se agregaron algunas validaciones a las entidades

---
+++ CreditCardApp.Api/Validators/CardValidator.cs
@@ -0,0 +1,23 @@
+﻿using CreditCardApp.Api.DTOs;
+using FluentValidation;
+
+namespace CreditCardApp.Api.Validators;
+
+public class CardValidator : AbstractValidator<CardDto>
+{
+    public CardValidator()
+    {
+        RuleFor(card => card.CardHolderName)
+            .NotEmpty().WithMessage("El nombre del titular no puede estar vacío.")
+            .MaximumLength(100).WithMessage("El nombre del titular no puede exceder los 100 caracteres.");
+
+        RuleFor(card => card.CardNumber)
+            .CreditCard().WithMessage("El número de tarjeta no es válido.");
+
+        RuleFor(card => card.CreditLimit)
+            .GreaterThan(0).WithMessage("El límite de la tarjeta debe ser mayor a 0.");
+
+        RuleFor(card => card.CurrentBalance)
+            .GreaterThanOrEqualTo(0).WithMessage("El saldo actual no puede ser negativo.");
+    }
+}
