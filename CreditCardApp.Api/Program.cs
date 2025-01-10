c5da1fa193256a00d78e3614f8d79267b9d43b37
snaven10 <funes@samuelfunes.com>
2025-01-09 22:27:05 -0600

    Se agrego el HealthCheck

--- CreditCardApp.Api/Program.cs
+++ CreditCardApp.Api/Program.cs
@@ -1,30 +1,64 @@
 using CreditCardApp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using FluentValidation;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Web;
+using HealthChecks.ApplicationStatus.DependencyInjection;
 using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Add DbContext
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddFluentValidation(fv =>
+{
+    fv.RegisterValidatorsFromAssemblyContaining<Program>();
+});
+// Registro del DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add AutoMapper
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Configuración de health checks
+builder.Services
+    .AddHealthChecks()
+    .AddApplicationStatus(name: "api_status", tags: new[] { "api" })
+    .AddSqlServer(
+        connectionString: builder.Configuration.GetConnectionString("DefaultConnection")!,
+        name: "sql",
+        failureStatus: HealthStatus.Degraded,
+        tags: new[] { "db", "sql", "sqlserver" })
+    .AddCheck<ServerHealthCheck>("server_health_check", tags: new[] { "custom", "api" });
 
-// Add FluentValidation
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+builder.Services.AddHealthChecksUI(options =>
+{
+    options.SetEvaluationTimeInSeconds(builder.Configuration.GetValue<int>("HealthChecksUI:EvaluationTimeInSeconds"));
+    options.MaximumHistoryEntriesPerEndpoint(50);
+})
+.AddInMemoryStorage();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("/", () => "¡Hola, mundo!");
+// Map HealthChecks endpoints
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.UseHealthChecksUI(config =>
+{
+    config.UIPath = "/health-ui";
+});
+
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -32,9 +66,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ CreditCardApp.Api/ServerHealthCheck.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Web;
+
+public class ServerHealthCheck : IHealthCheck
+{
+    private readonly IConfiguration _configuration;
+    private readonly long _minimumFreeDiskSpaceInBytes;
+
+    public ServerHealthCheck(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _minimumFreeDiskSpaceInBytes = _configuration.GetValue<long>("HealthChecks:MinimumFreeDiskSpace");
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var unhealthyDrives = new List<string>();
+            foreach (var drive in DriveInfo.GetDrives().Where(d => d.IsReady))
+            {
+                if (drive.AvailableFreeSpace < _minimumFreeDiskSpaceInBytes)
+                {
+                    unhealthyDrives.Add($"{drive.Name}: {drive.AvailableFreeSpace} bytes disponibles.");
+                }
+            }
+
+            if (unhealthyDrives.Count == 0)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("Todos los discos tienen suficiente espacio."));
+            }
+            else
+            {
+                return Task.FromResult(HealthCheckResult.Degraded($"Problemas en los discos: {string.Join(", ", unhealthyDrives)}"));
+            }
+        }
+        catch (Exception ex)
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy($"Ocurrió un error al verificar los discos: {ex.Message}"));
+        }
+    }
+}
