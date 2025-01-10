4cf1e61cff1e155fe086a3cfdd296f5484516bc9
snaven10 <funes@samuelfunes.com>
2025-01-09 19:34:42 -0600

    Agregar entidades nuevas al contexto

--- CreditCardApp.Api/Data/ApplicationDbContext.cs
+++ CreditCardApp.Api/Data/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace CreditCardApp.Api.Data
 
         // DbSet para la entidad Card
         public DbSet<Card> Cards { get; set; } = null!;
+        public DbSet<Purchase> Purchases { get; set; } = null!;
+        public DbSet<Payment> Payments { get; set; } = null!;
     }
 }
