f4d01c09361e23382539c52fe130d1c65cc909ef
snaven10 <funes@samuelfunes.com>
2025-01-09 20:15:00 -0600

    Creacion de controller de Transaction

---
+++ CreditCardApp.Api/Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CreditCardApp.Api.Data;
+using CreditCardApp.Api.DTOs;
+using CreditCardApp.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditCardApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public TransactionController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/transaction
+        [HttpGet]
+        public async Task<IActionResult> GetTransactions()
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+            var transactionDtos = _mapper.Map<List<TransactionDto>>(transactions);
+            return Ok(transactionDtos);
+        }
+    }
+}
