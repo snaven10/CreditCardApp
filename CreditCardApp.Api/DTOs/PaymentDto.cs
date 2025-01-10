a8fdc6b041fd1d087648baaaa75c06ef32e8a6d7
snaven10 <funes@samuelfunes.com>
2025-01-09 19:36:55 -0600

    Normaliza finales de línea (LF a CRLF)

--- CreditCardApp.Api/DTOs/PaymentDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CreditCardApp.Api.DTOs
-{
-    public class PaymentDto
-    {
-        public int CardId { get; set; }
-        public DateTime PaymentDate { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
